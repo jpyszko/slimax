31020cad749892eacd478531e434921c63b97a2a
jpyszko <yasper85@gmail.com>
2021-08-16 13:25:35 +0200

    podlaczenie biblioteki Qt

--- main.cpp
+++ main.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
+#include <QApplication>
+#include <QPushButton>
 #include "engine/SimulationBuilder.h"
 #include "engine/SimulationRunner.h"
 #include "console/ConsoleNotificator.h"
 
-int main() {
-    SimulationBuilder builder;
-    SimulationBuilder &simulationBuilder = builder.simulation(10, 200, 500)
-            .addSnail("maciek", ROMAN_SNAIL, 5)
-            .addSnail("maciek2", ROMAN_SNAIL, 30)
-            .addSnail("maciek3", ROMAN_SNAIL, 20)
-            .addSnail("zenek", TURKISH_SNAIL, 10)
-            .addSnail("wojtek", GARDEN_SNAIL, 10)
-            .addPlant("salata", LETTUCE, 10)
-            .addPlant("trawa", GRASS, 30)
-            .addPlant("marchewka", CARROT, 12);
-    shared_ptr<Notificator> notificator = make_shared<ConsoleNotificator>();
-    SimulationRunner runner(notificator);
-    runner.load(simulationBuilder);
-    SimulationResult result = runner.run();
+int main(int argc, char *argv[]) {
 
-    std::cout << result;
-
-    return 0;
+    QApplication a(argc, argv);
+    QPushButton button("Hello world!", nullptr);
+    button.resize(200, 100);
+    button.show();
+    return QApplication::exec();
+//    SimulationBuilder builder;
+//    SimulationBuilder &simulationBuilder = builder.simulation(10, 200, 500)
+//            .addSnail("maciek", ROMAN_SNAIL, 5)
+//            .addSnail("maciek2", ROMAN_SNAIL, 30)
+//            .addSnail("maciek3", ROMAN_SNAIL, 20)
+//            .addSnail("zenek", TURKISH_SNAIL, 10)
+//            .addSnail("wojtek", GARDEN_SNAIL, 10)
+//            .addPlant("salata", LETTUCE, 10)
+//            .addPlant("trawa", GRASS, 30)
+//            .addPlant("marchewka", CARROT, 12);
+//    shared_ptr<Notificator> notificator = make_shared<ConsoleNotificator>();
+//    SimulationRunner runner(notificator);
+//    runner.load(simulationBuilder);
+//    SimulationResult result = runner.run();
+//
+//    std::cout << result;
+//
+//    return 0;
 }
